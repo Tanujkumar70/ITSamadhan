315abf3581792d592eeac90d8da5f893e9f482bc
TanujKumar <Tanujkumar84743@gmail.com>
2025-08-22 10:50:02 +0530

    Add .gitignore and clean ignored files

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,32 +1,11 @@
-using System.Diagnostics;
-using EMP.Models;
 using Microsoft.AspNetCore.Mvc;
 
-namespace EMP.Controllers
+namespace ITSamadhan.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
-
-        public IActionResult Index()
-        {
-            return View();
-        }
-
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
+        public IActionResult Index() => View();
+        public IActionResult About() => View();
+        public IActionResult Contact() => View();
     }
 }
